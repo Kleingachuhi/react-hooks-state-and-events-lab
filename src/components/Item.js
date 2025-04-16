6f4a5f6cec1f3dd59d7e1bd754995773fef1b211
Klein <klein.gachuhi@student.moringaschool.com>
2025-04-16 17:54:33 +0300

    Reactions

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isAdded, setToAdd] = useState("Add to Cart")
+  const [myList, setMyList] = useState('')
+
+  function handleAdding(){
+    // setToAdd( prevAdd => 
+    //   prevAdd === "Add to Cart" ? "Remove from Cart" : "Add to Cart"
+    // )
+setMyList( prevList => 
+  prevList === '' ? "in-cart" : ''
+)
+
+if (myList === "in-cart"){
+  setToAdd('Add to Cart')
+}
+else if (myList === "") {
+  setToAdd('Remove From Cart')
+}
+  }
   return (
-    <li className="">
+    <li className={myList} >
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAdding} >{isAdded}</button>
     </li>
   );
 }
